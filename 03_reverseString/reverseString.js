b4cebb2f71dcdb05f8e3b23ce08a57abf7ccc394
mehrit2003 <mehrit2003@gmail.com>
2023-02-19 10:57:11 +0100

    .join() the reversedSplitedArray and assign it to let finalreversedString'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,9 @@
 const reverseString = function(text) {
     let splitStr = text.split("");
     let reversedSplitedArray = splitStr.reverse();
+    let finalReversedString = reversedSplitedArray.join();
     
-    return reversedSplitedArray;
+    return finalReversedString;
 };
 
 // Do not edit below this line
