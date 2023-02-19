9e08354699aaa3fc599828e5b475bb649f4f88dd
mehrit2003 <mehrit2003@gmail.com>
2023-02-19 11:00:02 +0100

    fix the .join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(text) {
-    let splitStr = text.split("");
-    let reversedSplitedArray = splitStr.reverse();
-    let finalReversedString = reversedSplitedArray.join();
+    let splitStr = text.split("");      //split the string argument into a new array
+    let reversedSplitedArray = splitStr.reverse(); // reverse the array
+    let finalReversedString = reversedSplitedArray.join(""); //join the reversedSplitedArray
     
     return finalReversedString;
 };
