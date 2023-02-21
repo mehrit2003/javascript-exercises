b14a44ae1047b735f0f8941a6a05a66c72bb959f
mehrit2003 <mehrit2003@gmail.com>
2023-02-21 08:21:12 +0100

    looping in arr and display indexOf(element)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, element ) {
+    
+    let arrLength = arr.length; 
+    console.log(arrLength);
+
+    for(let i = 0; i <arrLength; i++){
+        
+        if(arr[i] === element){    
+            console.log(arr.indexOf(element))}; 
+
+            //remove 1 item starting at indexOf our argument;
+            arr.splice(arr.indexOf(element), 1);
+            
+            console.log(arrLength);
+            
+            return arr;
+            
+    }
+
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//Googling: remove item from array by Value
