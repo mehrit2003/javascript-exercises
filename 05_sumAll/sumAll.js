7005b1e3aa80123930168172bbbacd6df6899b03
mehrit2003 <mehrit2003@gmail.com>
2023-02-22 10:52:22 +0100

    Check if arguments are Integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
 
+const sumAll = function (x, y) {
+  //Check if both numbers are Integer
+  if(!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";;
+
+  console.log(x);
+ 
+    
+  return sum;
 };
 
 // Do not edit below this line
