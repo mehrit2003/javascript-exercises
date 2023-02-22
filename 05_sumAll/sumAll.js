5688f0f0eaba8a31dfc86fcaec289e9d7d9e0f4b
mehrit2003 <mehrit2003@gmail.com>
2023-02-22 11:09:41 +0100

    Working with large number first: X > Y: Declare new variable const temp to swap x and y values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,33 @@
+//Conditions Order is IMPORTANT( if() )
 
 const sumAll = function (x, y) {
   //Check if both numbers are Integer
-  if(!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";;
+  if(!Number.isInteger(x) || !Number.isInteger(y)) 
+  return "ERROR";;
 
   //Check if both numbers are not Negative
-  if( x < 0 || y < 0) return "ERROR";
+  if( x < 0 || y < 0) 
+  return "ERROR";
   console.log(x);
 
-  //increasing sum with for loop(TOP Solution)
+    //Working with large number first  x > y (TOP Solution)
+    if(x > y){
+      const temp = x;   //declare temporary variable to swap x and y values
+      x = y;
+      y = temp;
+  
+    }
+
+
+
+  //increasing sum with for loop
   let sum = 0;
   for(let i = x; i < y + 1; i++){
     sum +=i;
   }
+
+
+  
  
     
   return sum;
