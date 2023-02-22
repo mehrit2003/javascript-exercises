c8cf160cfaecd5a01823e7ff11bed3829ee1e835
mehrit2003 <mehrit2003@gmail.com>
2023-02-22 10:36:35 +0100

    Added TOP Solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,18 @@
-const removeFromArray = function(arr, element ) {
-    
-    let arrLength = arr.length; 
-    console.log(arrLength);
-
-    for(let i = 0; i <arrLength; i++){
-        
-        if(arr[i] === element){    
-            console.log(arr.indexOf(element))}; 
-
-            //remove 1 item starting at indexOf our argument;
-            arr.splice(arr.indexOf(element), 1);
-            
-            console.log(arrLength);
-            
-            return arr;
-            
-    }
-
-
-};
+const removeFromArray = function (array, ...args) {
+    // create a new empty array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+      // push every element into the new array
+      // UNLESS it is included in the function arguments
+      // so we create a new array with every item, except those that should be removed
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    // and return that array
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
