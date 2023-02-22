c22b294091fc30d0f8aa3dd3c28c7c676fd82485
mehrit2003 <mehrit2003@gmail.com>
2023-02-22 13:35:09 +0100

    Added TOP Solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,26 @@
 /* Formula  Fahrenheit to Celsius: (32°F − 32) × 5/9 = 0°C
    Formula  Celsius to Fahrenheit: (32°C × 9/5) + 32 = 89.6°F 
 
+  using Math.round()
+
 */
 
 
-const convertToCelsius = function(fehr) {
-  return (fehr - 32) * 5/9;
+/*const convertToCelsius = function(fehr) {
+  return Math.round((fehr - 32) * (5/9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(cels) {
-  return (cels * 9/5) + 32;
+  return (((cels * 9/5) + 32) * 10) / 10;
+};*/
+
+//TOP Solution
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
+};
+
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
