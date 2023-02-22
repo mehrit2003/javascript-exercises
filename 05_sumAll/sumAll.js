034150eaf9ccf062e81e8f54f2eb8c64ead3cafa
mehrit2003 <mehrit2003@gmail.com>
2023-02-22 10:56:11 +0100

    Check if both arguments are \!Negative Numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,8 @@ const sumAll = function (x, y) {
   //Check if both numbers are Integer
   if(!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";;
 
+  //Check if both numbers are not Negative
+  if( x < 0 || y < 0) return "ERROR";
   console.log(x);
  
     
