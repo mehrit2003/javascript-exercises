3b8e5c3d8e632c7ba4f6dabd7064d64bca1a9443
mehrit2003 <mehrit2003@gmail.com>
2023-02-22 12:09:09 +0100

    check if year is dividable by 100 return false unless dividable by 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,20 @@
 const leapYears = function(year) {
 
     //check if dividable by 4
-    if(year % 4 == 0) return true;
+    if(year % 4 == 0  ){
+        if(year % 100 ==0){ //if year is dividable by 100 return false unless it is dividable by 400
+            if(year % 400 == 0){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }else{
+        return false;
+    }
+
+    
+    
 
 };
 
