9ac0e886b7a9233edb21f02fbe4d7df4a227ef25
mehrit2003 <mehrit2003@gmail.com>
2023-02-22 12:49:53 +0100

    return formulas in convert functions body with parameters (fehr) and (cels)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+/* Formula  Fahrenheit to Celsius: (32°F − 32) × 5/9 = 0°C
+   Formula  Celsius to Fahrenheit: (32°C × 9/5) + 32 = 89.6°F 
+
+*/
+
+
+const convertToCelsius = function(fehr) {
+  return (fehr - 32) * 5/9;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return (cels * 9/5) + 32;
 };
 
 // Do not edit below this line
