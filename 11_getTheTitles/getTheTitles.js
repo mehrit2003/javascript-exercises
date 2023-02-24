8b820d0758623af07f29242f58e67eaaf791cd78
mehrit2003 <mehrit2003@gmail.com>
2023-02-24 11:34:06 +0100

    use .map() to return new array of values  ValueArray = array.map(element => element.value)

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+//using .map() to return new array
 
+const getTheTitles = function(books) {
+    console.log(books.map(book => book.title))
+    return books.map(book => book.title);
+    
 };
 
 // Do not edit below this line
