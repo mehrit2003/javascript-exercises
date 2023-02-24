94cf8c7e143063f9596180f63f684ead98120e6b
mehrit2003 <mehrit2003@gmail.com>
2023-02-24 10:18:49 +0100

    change string to lowerCase and replace non alphabetic characteres/regexp/  THEN compare it with it's split().reverse().join()

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+/*FCC Solution: Check RegExp
+https://www.freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/
+*/
 
+const palindromes = function (str) {
+    // str.tolowerCase and replace() any non alphabetic character with empry string "":RegExp
+    const processedStr = str.toLowerCase().replace(/[^a-z]/g,"");
+    if(processedStr.split("").reverse().join("") === processedStr){
+        return true;
+    }else{
+        return false;
+    }
+   //split() reverse() and join() the new string
+    console.log(processedStr);
 };
 
 // Do not edit below this line
